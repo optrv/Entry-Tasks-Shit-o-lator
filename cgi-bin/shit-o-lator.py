cb12af4c4aab886125d7bb9c0550a56c882ac2d1
Oleksandr Petrov <garazd@MacBook-Pro-Garazd.local>
2016-07-14 14:50:16 +0300

    The return of 'Zero division checking'.
    The return of forgotten ability to check 'zero division', that was
    present in the previous version.

--- cgi-bin/shit-o-lator.py
+++ cgi-bin/shit-o-lator.py
@@ -12,33 +12,55 @@ print("<meta charset=\"utf-8\">")
 print()
 print("<style>h1,p{text-align:center;}</style>")
 
+# Check the zero
+def zero_check(dig2):
+    if oper == '/' and dig2 == '0':
+        print("<br><h1>You can't divide on zero!</h1>")
+        print("<p><a href='javascript:window.history.back()'>Try again!</a><br>or<br>")
+        print("<a href='/'>Go the main menu</a></p>")
+        return False
+    else:
+        return True
+
 # Check the digits
 def digit_check(dig, dig2):
-    try:
-        float(dig)
-        float(dig2)
-        return (True)
-    except (ValueError):
-        print("<br><h1>Please, input the digits!</h1>")
-        print("<p><a href='javascript:window.history.back()'>Try again!</a><br>or<br>")
-        print("<a href='/'>Go the main menu</a></p>")        
+    if zero_check(dig2):
+        try:
+            float(dig)
+            float(dig2)
+            return (True)
+        except (ValueError):
+            print("<br><h1>Please, input the digits!</h1>")
+            print("<p><a href='javascript:window.history.back()'>Try again!</a><br>or<br>")
+            print("<a href='/'>Go the main menu</a></p>")                
+
+# Output the result
+def output(summ):
+    if summ - int(summ) == 0:
+        print("<br><br><h1>The result is: {}</h1>".format(int(summ)))
+        print("<p><a href='javascript:window.location = document.referrer;'>Try again!</a><br>or<br>")
+        print("<a href='/'>Go to the main menu</a></p>")    
+    else:
+        print("<br><br><h1>The result is: {0:.2f}</h1>".format(summ))
+        print("<p><a href='javascript:window.location = document.referrer;'>Try again!</a><br>or<br>")
+        print("<a href='/'>Go to the main menu</a></p>")    
 
 # Calculate the digits
 if digit_check(dig1,dig2):
-    oper_kind = {'+': float(dig1) + float(dig2),
-                '-': float(dig1) - float(dig2),
-                '*': float(dig1) * float(dig2),
-                '/': float(dig1) / float(dig2)}
-    for n in oper_kind:
-        if n == oper:
-            sum = oper_kind[n]
-            
-# Output the result    
-if sum - int(sum) == 0:
-    print("<br><br><h1>The result is: {}</h1>".format(int(sum)))
-    print("<p><a href='javascript:window.location = document.referrer;'>Try again!</a><br>or<br>")
-    print("<a href='/'>Go to the main menu</a></p>")    
-else:
-    print("<br><br><h1>The result is: {0:.2f}</h1>".format(sum))
-    print("<p><a href='javascript:window.location = document.referrer;'>Try again!</a><br>or<br>")
-    print("<a href='/'>Go to the main menu</a></p>")   
+    try:
+        oper_kind = {'+': float(dig1) + float(dig2),
+                    '-': float(dig1) - float(dig2),
+                    '*': float(dig1) * float(dig2),
+                    '/': float(dig1) / float(dig2)}
+        for n in oper_kind:
+            if n == oper:
+                summ = oper_kind[n]
+                output(summ)
+    except (ZeroDivisionError):
+        oper_kind = {'+': float(dig1) + float(dig2),
+                    '-': float(dig1) - float(dig2),
+                    '*': float(dig1) * float(dig2)}
+        for n in oper_kind:
+            if n == oper:
+                summ = oper_kind[n]
+                output(summ)
